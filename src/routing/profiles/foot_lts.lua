009a9d5a8a627a078839afb58262beb41591ab6f
Matt Bhagat-Conway <mwbc@unc.edu>
2023-03-01 18:35:11 -0500

    store rates as meter-equivalents per second, not kilometer-equivalents per hour, so that on safe facilities weight == duration

--- src/routing/profiles/foot_lts.lua
+++ src/routing/profiles/foot_lts.lua
@@ -407,8 +407,11 @@ function process_way(profile, way, result)
   -- divide by the multiplier... because these are rates (think km/h but instead generalized cost / h)
   -- we divide by the multiplier - we want to reduce the rate on low-quality facilities.
   -- Note that we are setting based on speed - rate is not set in default OSRM foot routing profile.
-  result.forward_rate = result.forward_speed / multiplier
-  result.backward_rate = result.backward_speed / multiplier
+  -- Divide by 3.6 so that rate is in (roughly) meter-equivalents per second, and thus weights are comparable to
+  -- durations. This should not affect the routing whatsoever. This conversion is done automatically within OSRM for
+  -- speeds.
+  result.forward_rate = result.forward_speed / multiplier / 3.6
+  result.backward_rate = result.backward_speed / multiplier / 3.6
 end
 
 function process_turn (profile, turn)
