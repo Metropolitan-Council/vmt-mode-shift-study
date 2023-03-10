a3ca5cc4baa8134d7e1e3423b9d76231df0bc65b
Matt Bhagat-Conway <matt@indicatrix.org>
2023-03-09 20:24:46 -0500

    fix readlink to handle files with spaces

--- src/routing/build_street_network.sh
+++ src/routing/build_street_network.sh
@@ -11,10 +11,11 @@ mkdir "${OUTPUT_DIR}"
 NET_NAME=`basename ${OUTPUT_DIR}`
 OSM_TARGET="${OUTPUT_DIR}/${NET_NAME}.osm.pbf"
 OSRM_TARGET="${OSM_TARGET%.osm.pbf}.osrm"
-
 # use readlink to get the absolute path, to avoid differences between macOS and Linux when
 # creating symlinks to a relative path in a subdirectory.
-ln -s `readlink -f "${OSM_FILE}"` "${OSM_TARGET}"
+OSM_FULL_PATH="$(readlink -f "${OSM_FILE}")"
+
+ln -s "${OSM_FULL_PATH}" "${OSM_TARGET}"
 osrm-extract -p "${PROFILE}" "${OSM_TARGET}"
 osrm-partition "${OSRM_TARGET}"
 osrm-customize "${OSRM_TARGET}"
