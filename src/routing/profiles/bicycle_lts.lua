3e73bf2c2ca5f1c2d95088a855282a0fba728a63
Matt Bhagat-Conway <matt@indicatrix.org>
2023-04-14 15:40:39 -0400

    Revert "remove references to car profile from bike profile"
    Turns out it is needed for max speed calculation.`
    
    This reverts commit 8cc4d4b19004f4ee1fb3ca9561b694d9354149ce.

--- src/routing/profiles/bicycle_lts.lua
+++ src/routing/profiles/bicycle_lts.lua
@@ -11,6 +11,7 @@ find_access_tag = require("lualib/access").find_access_tag
 limit = require("lualib/maxspeed").limit
 Measure = require("lualib/measure")
 walk_profile = require("foot_lts")
+car_profile = require("car_traffic")
 LTS = require("lts")
 
 function setup()
@@ -228,7 +229,8 @@ function setup()
       'construction'
     },
 
-    walk_profile = walk_profile.setup()
+    walk_profile = walk_profile.setup(),
+    car_profile = car_profile.setup()
   }
 end
 
