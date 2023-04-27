8ab23d43492db976e19f17f604af84e23eaf9ac1
Greg Erhardt <greg.erhardt@uky.edu>
2023-04-27 10:31:06 +0200

    Add observed trip attributes to route visualizer.

--- data_viz/route_mapper.py
+++ data_viz/route_mapper.py
@@ -8,7 +8,7 @@ from shapely.geometry import Point
 
 class RouteMapper(object):
 
-    def __init__(self, tbi_df, car_gdf, walk_gdf, bike_gdf, transit_gdf, observed_gdf):
+    def __init__(self, tbi_df, car_gdf, walk_gdf, bike_gdf, transit_gdf, transit_grouped_gdf, observed_gdf):
         
         # a dataframe of the processed TBI trips
         self.tbi = tbi_df
@@ -18,6 +18,7 @@ class RouteMapper(object):
         self.walk = walk_gdf
         self.bike = bike_gdf
         self.transit = transit_gdf
+        self.transit_grouped = transit_grouped_gdf
         self.observed = observed_gdf
     
         # Categorical color scheme for various travel modes
@@ -167,18 +168,89 @@ class RouteMapper(object):
         return(route_map)
         
     
+    def create_legend_html(self, trip_id):
+    
+        # Header
+        tbi_trip = self.tbi[self.tbi['trip_id']==trip_id].iloc[0]
+        legend_html = '''
+        <div style="position: fixed; 
+            background-color: #FFFFFF;
+            bottom: 50px; right: 50px; width: 250px; height: 430px; 
+            border:2px solid grey; z-index: 9999; font-size:10px;">
+            
+            &nbsp;<b>Trip ID: ''' + str(trip_id) + ''' </b><br>
+            &nbsp;<i class="fa fa-circle" style="color:#E46707"></i>&nbsp;Observed<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mode: ''' + tbi_trip['mode'] + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;O Purpose: ''' + tbi_trip['o_purpose'] + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;D Purpose: ''' + tbi_trip['d_purpose'] + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Num Travelers: ''' + tbi_trip['num_travelers'] + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Age: ''' + tbi_trip['age'] + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Day of Week: ''' + tbi_trip['travel_dow'] + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Departure Time: ''' + tbi_trip['depart_time'] + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Arrival Time: ''' + tbi_trip['arrive_time'] + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Distance (mi): ''' + str(round(tbi_trip['distance'],2)) + ''' <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (min): ''' + str(tbi_trip['duration']) + ''' <br>
+            '''
+            
+        # Walk attributes 
+        walk_trip = self.walk[self.walk['trip_id']==trip_id].iloc[0]
+        legend_html = legend_html + '''
+            &nbsp;<i class="fa fa-circle" style="color:#006400"></i>&nbsp;Walking<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Distance (mi): ''' + str(round(walk_trip['distance_meters']/1609.34 ,2)) + '''<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (min): ''' + str(round(walk_trip['duration_seconds']/60. ,2)) + '''<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gen Cost (min): ''' + str(round(walk_trip['weight']/60. ,2)) + '''<br>
+            '''
+        
+        # Bike attributes        
+        bike_trip = self.bike[self.bike['trip_id']==trip_id].iloc[0]
+        legend_html = legend_html + '''
+            &nbsp;<i class="fa fa-circle" style="color:#00008B"></i>&nbsp;Biking<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Distance (mi): ''' + str(round(bike_trip['distance_meters']/1609.34 ,2)) + '''<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (min): ''' + str(round(bike_trip['duration_seconds']/60. ,2)) + '''<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gen Cost (min): ''' + str(round(bike_trip['weight']/60. ,2)) + '''<br>
+            '''
+        
+        # car attributes        
+        car_trip = self.car[self.car['trip_id']==trip_id].iloc[0]
+        legend_html = legend_html + '''
+            &nbsp;<i class="fa fa-circle" style="color:#8B4513"></i>&nbsp;Car<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Distance (mi): ''' + str(round(car_trip['distance_meters']/1609.34 ,2)) + '''<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (min): ''' + str(round(car_trip['duration_seconds']/60. ,2)) + '''<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gen Cost (min): ''' + str(round(car_trip['weight']/60. ,2)) + '''<br>
+            '''
+        
+        # Transit attributes -- only if there is a valid path
+        transit_trips = self.transit_grouped[self.transit_grouped['trip_id']==trip_id]
+        if len(transit_trips) > 0: 
+            transit_trip = transit_trips.iloc[0]
+            legend_html = legend_html + '''
+                &nbsp;<i class="fa fa-circle" style="color:#4B0082"></i>&nbsp;Transit<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Start Time: ''' + str(transit_trip['start_time_dt'].time()) + '''<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;End Time: ''' + str(transit_trip['end_time_dt'].time()) + '''<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (min): ''' + str(round(transit_trip['duration'],2)) + '''<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Legs:''' + str(transit_trip['leg_index']) + ''' <br>
+                '''
+        else:
+            legend_html = legend_html + '''
+                &nbsp;<i class="fa fa-circle" style="color:#4B0082"></i>&nbsp;Transit<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No Path Found<br>
+                '''
+            
+
+        # Footer
+        legend_html = legend_html + '''
+        </div>
+        '''
+        
+        return legend_html
+    
     def map_trip(self, trip_id="Random"):
         # plot the routes on a map
         
         if trip_id=="Random":
             trip_id = self.tbi.sample(n=1)['trip_id'].iloc[0]
         trip = self.tbi[self.tbi['trip_id']==trip_id]
-        
-            
-        # tooltip current broken when adding as arugment below in folium.GeoJson
-        #tooltip=folium.features.GeoJsonTooltip(fields=['mode', 'o_purpose', 'd_purpose', 'num_travelers', 'distance', 'speed_mph', 'vmt', 'age'],
-        #                                     aliases=['Mode', 'O purpose', 'D purpose', '# of travelers', 'Distance', 'Speed (mph)', 'VMT', 'Age'])
-            
+                    
         # instantiate a global Leaflet map object using Folium
         route_map = folium.Map(location=[44.9778,-93.2650], tiles="CartoDB positron", zoom_start=10)
         
@@ -188,35 +260,8 @@ class RouteMapper(object):
         route_map = self.add_transit_route(route_map, trip_id)
         route_map = self.add_observed_route(route_map, trip_id)
 
-        # create html string for legend
-        legend_html = '''
-        <div style="position: fixed; 
-            background-color: #FFFFFF;
-            bottom: 50px; right: 50px; width: 200px; height: 275px; 
-            border:2px solid grey; z-index: 9999; font-size:10px;">
-            &nbsp;<b>Trip ID: ''' + str(trip_id) + ''' </b><br>
-            &nbsp;<i class="fa fa-circle" style="color:#006400"></i>&nbsp;Walking<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspDistance (m): <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspDuration (sec): <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspGen Cost (sec): <br>
-            &nbsp;<i class="fa fa-circle" style="color:#00008B"></i>&nbsp;Biking<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Distance (m): <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (sec): <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gen Cost (sec): <br>
-            &nbsp;<i class="fa fa-circle" style="color:#8B4513"></i>&nbsp;Driving<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Distance (m): <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (sec): <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gen Cost (sec): <br>
-            &nbsp;<i class="fa fa-circle" style="color:#4B0082"></i>&nbsp;Transit<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Start Time: <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;End Time: <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (sec): <br>
-            &nbsp;<i class="fa fa-circle" style="color:#E46707"></i>&nbsp;Observed<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Duration (sec): <br>
-        </div>
-        '''
-        
         # use branca to add legend to the map
+        legend_html = self.create_legend_html(trip_id)
         legend = branca.element.Element(legend_html)
         route_map.get_root().html.add_child(legend)
         
