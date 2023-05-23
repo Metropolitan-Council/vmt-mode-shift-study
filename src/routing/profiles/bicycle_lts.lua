c61be3e78de2bd89df10f9228f48a929f3ee51eb
Matt Bhagat-Conway <matt@indicatrix.org>
2023-05-23 18:38:21 -0400

    syntax fixes in bike/foot lts

--- src/routing/profiles/bicycle_lts.lua
+++ src/routing/profiles/bicycle_lts.lua
@@ -15,7 +15,7 @@ car_profile = require("car_traffic")
 LTS = require("lts")
 
 function setup()
-  local default_speed = 14 * 1.609
+  local default_speed = 12 * 1.609
   local walking_speed = 4
 
   return {
@@ -658,14 +658,14 @@ end
 function lts_weighter(profile, way, result, data)
   if data.lts == 2 then
     -- 10% penalty for LTS 2 vs 1
-    if result.forward_speed > 0
+    if result.forward_speed > 0 then
       result.forward_speed = result.forward_speed / 1.1
       result.forward_rate = result.forward_speed / 3.6 / 1.1
     else
-      result.foward_rate = -1
+      result.forward_rate = -1
     end
 
-    if result.backward_speed > 0
+    if result.backward_speed > 0 then
       result.backward_speed = result.backward_speed / 1.1
       result.backward_rate = result.backward_speed / 3.6 / 1.1
     else
@@ -674,13 +674,13 @@ function lts_weighter(profile, way, result, data)
   else
     --  assert(data.lts == 1, "LTS not 1 when it should be at way" .. way:id())
     
-    if result.forward_speed > 0
+    if result.forward_speed > 0 then
       result.forward_rate = result.forward_speed / 3.6
     else
       result.forward_rate = -1
     end
 
-    if result.backward_speed > 0
+    if result.backward_speed > 0 then
       result.backward_rate = result.backward_speed / 3.6
     else
       result.backward_rate = -1
@@ -728,14 +728,14 @@ function process_way(profile, way, result)
     walk_profile.process_way(profile.walk_profile, way, result)
 
     -- and apply a 25% speed and weight penalty to account for walking the bike
-    if result.forward_speed > 0
+    if result.forward_speed > 0 then
       assert(result.forward_rate > 0)
       result.forward_speed = result.forward_speed / 1.25
       result.forward_rate = result.forward_rate / 1.25
       result.forward_mode = mode.pushing_bike
     end
 
-    if result.backward_speed > 0
+    if result.backward_speed > 0 then
       assert(result.backward_rate > 0)
       result.backward_speed = result.backward_speed / 1.25
       result.backward_rate = result.backward_rate / 1.25
