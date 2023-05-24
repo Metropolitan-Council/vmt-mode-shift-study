242cc2a630779ecc28c0f743435dd772bb5046b0
yianzhang14 <78504318+yianzhang14@users.noreply.github.com>
2023-05-24 13:03:33 -0400

    fix for lts histogram

--- src/analysis_tool/steps/feasible_steps/bike_trip_details.py
+++ src/analysis_tool/steps/feasible_steps/bike_trip_details.py
@@ -57,7 +57,7 @@ class BikeHighLTSDistStep(ContinuousStep):
     
     def get_summary_figure(self):
         fig, ax = plot_mode_density(self.df, [(self.mode, self.column_name), (Mode.CAR, self.column_name)], percentile=self.cutoff) 
-        ax.set_xlim(left=0, right=500)
+        ax.set_xlim(left=0, right=1)
         plt.title(r"% of bike trip on high LTS routes for canonical bike and car trips")
         return fig, ax
     
