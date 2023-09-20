8a1a0de8e0ac74f9b3dd4a8880686f38605436e2
Matt Bhagat-Conway <matt@indicatrix.org>
2023-09-20 09:34:41 -0400

    add script to route ebike scenario

---
+++ src/routing/route_bicycle_ebike.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This encapsulates the routing options for the bicycle ebike scenario. It takes arguments for the path where the TBI is stored,
+# directory where all the networks are stored,
+# and the directory where output should go.
+
+set -ex
+
+TBI_DIR="$1"
+NET_DIR="$2"
+OUT_DIR="$3"
+
+routing="$(dirname "$0")"
+
+# get the git commit hash
+COMMIT_HASH=$(cd "$routing"; git rev-parse HEAD | head -c 5)
+
+OUT_FILE="${OUT_DIR}/bike-ebike-${COMMIT_HASH}.parquet"
+SEGMENTS_FILE="${OUT_DIR}/bike-ebike-${COMMIT_HASH}-segments.parquet"
+
+if [ ! -e "$OUT_FILE" -o -e "$SEGMENTS_FILE" ]; then
+    echo "Output files already exist!"
+else
+    # no reason to write segments; everything is LTS 1
+    julia --project="$routing" -t auto "${routing}/route.jl" "${TBI_DIR}/tbi_cleaned.csv" "${NET_DIR}/bike-ebike/bike-ebike.osrm" "$OUT_FILE" # --bike-lts "$SEGMENTS_FILE"
+fi
