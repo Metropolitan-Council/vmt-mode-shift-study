0277d146357834859a695769d2f70ca63c83e862
Matt Bhagat-Conway <matt@indicatrix.org>
2023-09-21 09:57:30 -0400

    document command to build scenario network

--- src/routing/build_transit_network.sh
+++ src/routing/build_transit_network.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# command used for scenario network: bash build_transit_network.sh --osrm-network ~/vmt-networks/walk-lts/walk-lts.osrm \
+#  -t 2413.5 ~/vmt-networks/transit-scenario.trjl \
+# /mnt/c/Users/mwbc/University\ of\ Kentucky/VMT\ Reduction\ Mode\ Shift\ -\ General/2-Re-Routing\ Analysis/GTFS/metro_transit_2019-10-16-scenario.zip \
+# /mnt/c/Users/mwbc/University\ of\ Kentucky/VMT\ Reduction\ Mode\ Shift\ -\ General/2-Re-Routing\ Analysis/GTFS/mvta-scenario.zip
+
 set -ex
 
 # may be relative path but that's ok
