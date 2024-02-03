88aab90395655b474ee30709d168db6572c83688
Matt Bhagat-Conway <matt@indicatrix.org>
2024-02-03 16:20:41 -0500

    add ped baseline test script

--- src/routing/profiles/test/test_ped_baseline.jl
+++ src/routing/profiles/test/test_ped_baseline.jl
@@ -1,9 +1,14 @@
-# This contains automated tests for the pedestrian network, to ensure the stress levels
-# are applied correctly. Run it with the path to the network.
+# This contains automated tests for the pedestrian network. It contains the following tests
+# - It spot-checks a number of edges (one in each category) to ensure they are assigned the correct pedestrian stress
+# - It plots the weight by pedestrian quality, to ensure that weights increase as quality decreases, and saves the result to
+#   ped_quality_weights.svg
+# - It checks the weight per meter of every edge to ensure it is consistent with the elevation changes, and plots the weight per meter
+#   as a function of elevation to ped_elevation_weights.svg
 
-import OSRM.Toolchain: OSRMToolchain, get_node_ids
+import OSRM.Toolchain: OSRMToolchain, get_node_ids, get_geometry
 import OSRM: OSRMInstance, route
-using Test, Geodesy
+using Test, Geodesy, Plots, StatsPlots, ArgParse, Rasters
+import CategoricalArrays: cut
 
 include("util.jl")
 
@@ -40,10 +45,7 @@ function get_ped_qual(indexed_toolchain, fr, to)
     end 
 end
 
-function main(network)
-    toolchain = OSRMToolchain(network)
-    it = IndexedToolchain(toolchain)
-
+function spot_check_quality_assignment(toolchain)
     @testset "Ped quality assignment" begin
         ##################
         ## HAS SIDEWALK ##
@@ -132,8 +134,101 @@ function main(network)
         ## OFF STREET ##
         ################
 
-        
+        # footway, way 183938393
+        @test get_ped_qual(it, 1943750611, 1943750614) == "high"
+
+        # cycleway, way 35090906
+        @test get_ped_qual(it, 248125876, 1735876478) == "high"
     end
 end
 
-main(ARGS[1])
+function plot_weights_by_quality(toolchain)
+    # ensure weights vary correctly by ped stress by plotting
+    # this won't be perfect due to rounding, elevation, surface penalties, etc.,
+    # but should look pretty good
+
+    pedquality = get_ped_qual.(Ref(toolchain), eachindex(toolchain.edge_based_nodes))
+    weight_per_meter = getproperty.(toolchain.edge_based_node_weights, :weight) ./ toolchain.edge_based_node_distances
+    sel = toolchain.edge_based_node_distances .> 0.1
+
+    violin(pedquality[sel], weight_per_meter[sel])
+    Plots.xlabel!("Pedestrian quality")
+    Plots.ylabel!("Weight per meter")
+    Plots.savefig("ped_quality_weights.svg")
+end
+
+function plot_weights_by_elevation(toolchain, elevation_file)
+    raster = Raster(elevation_file);
+    weight_per_meter = Float64[]
+    percent_steep = Float64[]
+    eids = Int64[]
+    
+    for (eidx, ebn) in pairs(toolchain.edge_based_nodes)
+        if eidx % 10000 == 0
+            @info "Processed $eidx edges"
+        end
+    
+        steep_dist = zero(Float64)
+    
+        geom = get_geometry(toolchain, ebn)::Vector{LatLon{Float64}}
+        for (p1, p2) in zip(geom[1:end-1], geom[2:end])
+            dist = euclidean_distance(p1, p2)
+            n_segments = max(round(Int64, dist / 10), 1)
+            steep_segments = 0
+    
+            Δlat = p2.lat - p1.lat
+            Δlon = p2.lon - p1.lon
+    
+            for segment in 1:n_segments
+                orig_frac = (segment - 1) / n_segments
+                dest_frac = segment / n_segments
+                start_elev_mm = get_elev(raster, p1.lat + orig_frac * Δlat, p1.lon + orig_frac * Δlon)
+                end_elev_mm = get_elev(raster, p1.lat + dest_frac * Δlat, p1.lon + dest_frac * Δlon)
+    
+                @assert !isnothing(start_elev_mm) && !isnothing(end_elev_mm)
+                if end_elev_mm > start_elev_mm
+                    slope_pct = (end_elev_mm - start_elev_mm) / 1000 / euclidean_distance(p1, p2) * 100
+                    if slope_pct > 10 && slope_pct < 35
+                        steep_segments += 1
+                    end
+                end
+            end
+    
+            steep_dist += steep_segments / n_segments * dist
+        end
+    
+        push!(weight_per_meter, toolchain.edge_based_node_weights[eidx].weight / toolchain.edge_based_node_distances[eidx])
+        push!(percent_steep, steep_dist / toolchain.edge_based_node_distances[eidx] * 100)
+        push!(eids, eidx)
+    end
+
+    sel = toolchain.edge_based_node_distances[eids] .> 0.5
+    percent_steep_binned = cut(min.(percent_steep[sel], 99.99), 0:10:100)
+    violin(percent_steep_binned, weight_per_meter[sel])
+
+    Plots.xlabel!("Percent with grade >10%")
+    Plots.ylabel!("Weight per meter")
+    Plots.savefig("ped_elevation_weights.svg")
+end
+
+function main(raw_args)
+    s = ArgParseSettings()
+
+    @add_arg_table! s begin
+        "network"
+            help = "OSRM network to use"
+        "elevation"
+            help = "Path to the combined.tif file containing elevation"
+    end
+
+    args = parse_args(raw_args, s)
+
+    toolchain = OSRMToolchain(args["network"])
+    it = IndexedToolchain(toolchain)
+
+    spot_check_quality_assignment(it)
+    plot_weights_by_quality(toolchain)
+    plot_weights_by_elevation(toolchain, args["elevation"])
+end
+
+main(ARGS)
